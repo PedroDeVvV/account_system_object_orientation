3f9a058521627452bcc1a68198eba4d0dc3b0760
PedroDeVvV <pedrohgs2004@gmail.com>
2024-03-15 17:13:28 -0300

    Add project files.

---
+++ Bank/UserAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace Bank
+{
+    internal class UserAccount
+    {
+        public int NumberAccount { get; private set; }
+        public string Name { get; private set; }
+        public double Value { get; private set; }
+
+        public UserAccount(int numberAccount, string name)
+        {
+            this.NumberAccount = numberAccount;
+            this.Name = name;
+        }
+
+        public UserAccount(int numberAccount, string name, double value)
+        {
+            this.NumberAccount = numberAccount;
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public void Deposito(double valorDeposito)
+        {
+            this.Value += valorDeposito;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            this.Value -= valorSaque;
+        }
+    }
+}
